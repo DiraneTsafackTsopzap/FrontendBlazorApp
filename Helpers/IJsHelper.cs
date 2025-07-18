09091b540507e656e83010bbdc4d2b3cb9b25e16
Tsafack Tsopzap Dirane <diraneserges@gmail.com>
2025-07-18 23:58:25 +0200

    first commit

---
+++ Helpers/IJsHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace FrontendApp.Helpers
+{
+    public static class IJsHelper
+    {
+        public static async ValueTask ToastSucces(this IJSRuntime JsRuntime, string message)
+        {
+            await JsRuntime.InvokeVoidAsync("FonctionShowToastr", "success", message);
+        }
+        public static async ValueTask ToastError(this IJSRuntime JsRuntime, string message)
+        {
+            await JsRuntime.InvokeVoidAsync("FonctionShowToastr", "error", message);
+        }
+    }
+}
---
+++ wwwroot/js/common.js
@@ -0,0 +1,25 @@
+﻿
+
+window.FonctionShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire({
+            icon: 'success',
+            title: 'Succès',
+            text: message,
+        });
+    }
+    if (type === "error") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Erreur',
+            text: message,
+        });
+    }
+    if (type === "info") {
+        Swal.fire({
+            icon: 'info',
+            title: 'Information',
+            text: message,
+        });
+    }
+};
